010b22fe8ea1e1800ff5162f280013afa6635cb1
yjgaia <yjdev88@gmail.com>
2024-09-28 14:08:42 +0900

    Refactor deployment script to copy ABI files to projects

--- README.md
+++ README.md
@@ -1,7 +1,15 @@
 # protocol
 
+```
+cd utils
+node --loader ts-node/esm ./copy-abi-files-to-projects.ts
+```
+
 ## Profiles
+
 - Base: 0x140AEc1F9fd26B8eB2832aF5C449f3D0C2EA0836
 
 ## Material Trade
-- Base Sepoila: [0x3Fcea379bCc53Df5062Bc9428FEFb5c495bE75be](https://sepolia.basescan.org/address/0x3Fcea379bCc53Df5062Bc9428FEFb5c495bE75be#code)
+
+- Base Sepoila:
+  [0x3Fcea379bCc53Df5062Bc9428FEFb5c495bE75be](https://sepolia.basescan.org/address/0x3Fcea379bCc53Df5062Bc9428FEFb5c495bE75be#code)
---
+++ utils/copy-abi-files-to-projects.ts
@@ -0,0 +1,47 @@
+import fs from "fs";
+import path from "path";
+
+enum Env {
+  APP = "app",
+  DENO = "deno",
+}
+
+const APP_ETHERS_PATH = "ethers";
+const APP_COMMON_PATH = "./common";
+const DENO_ETHERS_PATH = "https://esm.sh/ethers@6.7.0";
+const DENO_COMMON_PATH = "./common.ts";
+
+const RELATIONS = {
+  "gaming/MaterialTrade": {
+    "gaiawar-interface/game/contracts/abi": Env.APP,
+  },
+  "gaming/MaterialV1": {
+    "gaiawar-interface/game/contracts/abi": Env.APP,
+  },
+};
+
+for (const [contract, relations] of Object.entries(RELATIONS)) {
+  for (const [destination, env] of Object.entries(relations)) {
+    const filename = path.basename(contract, path.extname(contract));
+    const abiSource = fs.readFileSync(
+      `../artifacts/contracts/${contract}.sol/${filename}.json`,
+      "utf-8",
+    );
+    fs.writeFileSync(`../../${destination}/${filename}.json`, abiSource);
+
+    const typeSource = fs.readFileSync(
+      `../typechain-types/contracts/${contract}.ts`,
+      "utf-8",
+    );
+    fs.writeFileSync(
+      `../../${destination}/${filename}.ts`,
+      typeSource.replace(
+        `} from "ethers";`,
+        `} from "${env === Env.APP ? APP_ETHERS_PATH : DENO_ETHERS_PATH}";`,
+      ).replace(
+        `} from "../../common";`,
+        `} from "${env === Env.APP ? APP_COMMON_PATH : DENO_COMMON_PATH}";`,
+      ),
+    );
+  }
+}
