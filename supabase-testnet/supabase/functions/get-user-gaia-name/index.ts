5ee4ba1a293eb80188ab2630753dab80f2454195
yjgaia <yjdev88@gmail.com>
2024-11-23 19:46:12 +0900

    Remove unused get-user-gaia-name function and its dependencies

--- supabase-testnet/supabase/functions/get-user-gaia-name/index.ts
+++
@@ -1,8 +0,0 @@
-import { serve } from "https://raw.githubusercontent.com/yjgaia/deno-module/refs/heads/main/api.ts";
-import { extractWalletAddressFromRequest } from "https://raw.githubusercontent.com/yjgaia/wallet-login-module/refs/heads/main/deno/auth.ts";
-
-serve(async (req) => {
-  const walletAddress = extractWalletAddressFromRequest(req);
-
-  throw new Error("Not implemented");
-});
