5b82177a421a88dd87b600483e876fcc6758a1eb
yjgaia <yjdev88@gmail.com>
2024-12-13 15:24:30 +0900

    Add script to whitelist materials in testnet using Metamask client

---
+++ scripts/testnet/interact/add-to-material-whitelist.ts
@@ -0,0 +1,43 @@
+import { Material } from "../../../typechain-types/index.js";
+import { MetamaskClient } from "hardhat_metamask_client";
+import { config } from "chai";
+import { ethers, network } from "hardhat";
+
+const CONSTRUCTION_ADDRESS = "0xA2033689D584EB0F5ca69490b27eF9B274f2F724";
+
+const materialAddresses = {
+  wood: "0xb1e50e052a2c5601BD92fddcc058ADDCFD44c6E7",
+  stone: "0x63c45014DE5F0CbA76bbbA93A64D3d2DFd4f71cF",
+  iron: "0x1605AE85E05B3E59Ae4728357DE39bAc81ed0277",
+  ducat: "0x8D90c83bD9DBf0DB9D715378Bf4B7f3F5Ec749e5",
+};
+
+async function main() {
+  const client = new MetamaskClient({
+    hardhatConfig: config,
+    networkName: network.name,
+    network,
+    ethers,
+  });
+  const signer = await client.getSigner();
+
+  const Material = await ethers.getContractFactory("Material", signer);
+
+  for (const [name, address] of Object.entries(materialAddresses)) {
+    const contract = Material.attach(address) as Material;
+
+    const tx = await contract.addToWhitelist(CONSTRUCTION_ADDRESS);
+    await tx.wait();
+
+    console.log(`Added Construction to whitelist of ${name}`);
+  }
+
+  console.log("Done!");
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
