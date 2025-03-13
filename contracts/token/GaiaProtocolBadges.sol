dad960694b0dc2623e610f011e2969ff95b0753d
yjgaia <yjdev88@gmail.com>
2025-03-13 12:02:26 +0900

    Add GaiaProtocolBadges contract with upgradeable functionality and access control

---
+++ contracts/token/GaiaProtocolBadges.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/utils/ReentrancyGuardUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+
+contract GaiaProtocolBadges is OwnableUpgradeable, ReentrancyGuardUpgradeable, UUPSUpgradeable {
+    function initialize() public initializer {
+        __Ownable_init(msg.sender);
+        __ReentrancyGuard_init();
+        __UUPSUpgradeable_init();
+    }
+
+    function _authorizeUpgrade(address newImplementation) internal virtual override onlyOwner {}
+}
